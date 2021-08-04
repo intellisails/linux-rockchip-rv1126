7aa177927d6b4767030ad13e7eb61314cfec3a78
Cai YiWei <cyw@rock-chips.com>
2021-08-04 17:11:56 +0800

    media: rockchip: ispp: fix page fault due to scl exit early
    Change-Id: I8a7499a5d6e10707269525f7a0aa7a01b5aae7d6
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

--- drivers/media/platform/rockchip/ispp/stream.c
+++ drivers/media/platform/rockchip/ispp/stream.c
@@ -2787,7 +2787,7 @@ static void nr_work_event(struct rkispp_device *dev,
 			 seq, readl(base + RKISPP_NR_ADDR_BASE_Y_SHD),
 			 readl(base + RKISPP_SHARP_WR_Y_BASE_SHD));
 
-		for (val = STREAM_S0; val <= STREAM_S2; val++) {
+		for (val = STREAM_S0; val <= STREAM_S2 && !is_fec_en; val++) {
 			stream = &vdev->stream[val];
 			if (stream->stopping && stream->ops->stop)
 				stream->ops->stop(stream);
@@ -3363,7 +3363,8 @@ void rkispp_isr(u32 mis_val, struct rkispp_device *dev)
 			continue;
 		if (stream->stopping &&
 		    stream->ops->is_stopped &&
-		    stream->ops->is_stopped(stream)) {
+		    (stream->ops->is_stopped(stream) ||
+		     dev->ispp_sdev.state == ISPP_STOP)) {
 			stream->stopping = false;
 			stream->streaming = false;
 			stream->is_upd = false;
